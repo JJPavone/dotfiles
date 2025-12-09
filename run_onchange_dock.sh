58d6fab1615e8770614e336b07cbc969ae1e893e
JJPavone <85761906+JJPavone@users.noreply.github.com>
2025-12-09 12:15:56 -0500

    Add files via upload

---
+++ run_onchange_dock.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+source "./dock_functions.sh"
+
+declare -a apps=(
+    '/Applications/iTerm.app'
+    '/Applications/Discord.app'
+    '/Applications/Arc.app'
+    '/Applications/1Password.app'
+    '/Applications/Wireshark.app'
+    '/Applications/Visual Studio Code.app'
+    '/Applications/SDM.app'
+    '/Applications/Slack.app'
+    '/Applications/Claude.app'
+    '/Applications/Spotify.app'
+);
+
+declare -a folders=(
+    ~/Downloads
+);
+
+clear_dock
+disable_recent_apps_from_dock
+
+for app in "${apps[@]}"; do
+    add_app_to_dock "$app"
+done
+
+for folder in "${folders[@]}"; do
+    add_folder_to_dock $folder
+done
+
+killall Dock
+
