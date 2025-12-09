1374fd4e1ff13d35d3b67091b55e8134066b7b64
JJPavone <85761906+JJPavone@users.noreply.github.com>
2025-12-09 13:06:14 -0500

    Add files via upload

---
+++ run_onchange_mac_pref.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Dock Settings
+echo "Setting Dock tile size to 42..."
+defaults write com.apple.dock tilesize -int 42
+
+echo "Moving Dock to right side of screen..."
+defaults write com.apple.dock orientation -string right
+
+echo "Removing Dock auto-hide delay..."
+defaults write com.apple.dock autohide-delay -float 0
+
+echo "Hiding recent applications in Dock..."
+defaults write com.apple.dock show-recents -bool false
+
+# Finder Settings
+echo "Hiding desktop icons..."
+defaults write com.apple.finder CreateDesktop -bool false
+
+echo "Showing path bar in Finder..."
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Restart Dock and Finder to apply changes
+echo "Restarting Dock and Finder..."
+killall Dock
+killall Finder
